2085777086b5f1d1061933ad3e1cea298bebcd8b
Adaś <adampekul@gmail.com>
2023-01-11 16:22:23 +0100

    created project, installed and configure tailwindcss

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { Input } from "./Components/Input";
+import "./style.css";
 
 function App() {
+  // const [imageUrl, setImageUrl] = useState(null);
+
+  // const onChange = (e) => {
+  //   const { files } = e.target;
+  //   setImageUrl(URL.createObjectURL(files[0]));
+  // };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Input />
+      {/* <input
+        type="file"
+        accept="image/png, image/jpeg"
+        onChange={(e) => onChange(e)}
+      />
+      <img src={imageUrl} alt="file" /> */}
     </div>
   );
 }
---
+++ src/Components/Input.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+export function Input() {
+  return <div className="text-3xl font-bold">Input</div>;
+}
+
+export default Input;
